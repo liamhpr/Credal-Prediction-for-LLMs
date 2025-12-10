e5f7a3c4cfa1cabbe13e3b90bddab980cd0134b3
Liam Heppner <l.heppner6@gmail.com>
2025-12-10 13:41:23 +0100

    10/12/2025

--- src/install.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import config 
-model = AutoModelForCausalLM.from_pretrained("facebook/opt-350m", cache_dir=config.hf_cache_dir)
-tokenizer = AutoTokenizer.from_pretrained("facebook/opt-350m", cache_dir=config.hf_cache_dir)
-
